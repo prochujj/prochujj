2ae42a814f785c9db5db1c29287a6260c23d497c
chiraphat _0067 <118461894+prochujj@users.noreply.github.com>
2026-02-03 15:31:28 +0700

    Add files via upload

---
+++ myhotel_0067.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 03, 2026 at 09:19 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `myhotel_0067`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `guestbook`
+--
+
+CREATE TABLE `guestbook` (
+  `id` int(4) NOT NULL,
+  `name` varchar(65) NOT NULL,
+  `email` varchar(65) NOT NULL,
+  `comment` text NOT NULL,
+  `datetime` varchar(65) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `guestbook`
+--
+
+INSERT INTO `guestbook` (`id`, `name`, `email`, `comment`, `datetime`) VALUES
+(67, 'conan', 'pung@gmail.com', '****', '14.49/3/2/2569'),
+(68, 'Chiraphat ', '2516ozone@gmail.com', '333', '26-02-03 08:58:18');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `guestbook`
+--
+ALTER TABLE `guestbook`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `guestbook`
+--
+ALTER TABLE `guestbook`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
